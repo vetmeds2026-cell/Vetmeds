16aa57a4ec28e1261894b7a42c8e6c8cc79d8ae4
vetmeds2026-cell <vetmeds2026@gmail.com>
2026-07-16 00:04:13 +0530

    Add SEO metadata, robots and sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.thevetmeds.in",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+  ];
+}
